3e08b5d324a8950c64ab23df88781bc02c5f755b
Artem <webgid.main@gmail.com>
2024-05-20 11:35:47 +0300

    work version

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 const path = require('path');
 //console.log(path.join(process.cwd(), `.env`))
-const path2 = process.cwd().indexOf("/home/ubuntu") > -1 ? "/home/ubuntu/terabit" : "D:\\OpenServer\\domains\\mechanika"
+const path2 = process.cwd().indexOf("/root/mechanika") > -1 ? "/root/mechanika" : "D:\\OpenServer\\domains\\mechanika"
 
 require('dotenv').config({path: path.join(path2, `.env`)});
 
